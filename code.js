6fa886fd98d26e4e5734fe023509026a0af36486
JosedaSilvaSoca <jose.da.silva.soca@gmail.com>
2022-07-19 22:02:20 -0300

    cmbio en JS

--- code.js
+++ code.js
@@ -1,5 +1,5 @@
 document.getElementById("demo").onclick = function() {myFunction()};
 
 function myFunction() {
-  document.getElementById("demo").innerHTML = "YOU CLICKED ME!";
+  document.getElementById("demo").innerHTML = "Has cliqueado";
 }
